bd578cb3325fc120253962d8ac18826030995647
Tyler Daerden <daerdentyler@gmail.com>
2023-03-30 13:02:28 +0200

    Ajout des Corrections Thierry CLOTURE

---
+++ Corrections_Thierry/Program.cs
@@ -0,0 +1,160 @@
+﻿#region 1.1 Nombre premier < à x en utilisant boucle while et une collection
+{
+    //int nombre;
+
+    //do
+    //{
+    //    Console.Write("Entrez un nombre entier : ");
+    //} while (!int.TryParse(Console.ReadLine(), out nombre));
+
+    //ArrayList premiers = new ArrayList();
+    ////2 est le premier nombre premier
+    //int valeur = 2;
+    //while (valeur < nombre)
+    //{
+    //    bool estPremier = true;
+    //    int racine = (int)Math.Sqrt(valeur);
+
+    //    for (int index = 0; estPremier && racine > 1 && (int)premiers[index]! <= racine; index++)
+    //    {
+    //        if (valeur % (int)premiers[index]! == 0) //unboxing object > int
+    //            estPremier = false;
+    //    }
+
+    //    if (estPremier)
+    //    {
+    //        premiers.Add(valeur); //boxing int > object
+    //    }
+
+    //    valeur++;
+    //}
+
+    //foreach (int premier in premiers)
+    //{
+    //    Console.WriteLine($"{premier} est un nombre premier!");
+    //}
+}
+#endregion
+
+#region 1.2 x premiers nombres premiers en utilisant une collection générique
+{
+    //int nombre;
+
+    //do
+    //{
+    //    Console.Write("Entrez un nombre entier : ");
+    //} while (!int.TryParse(Console.ReadLine(), out nombre));
+
+    //List<int> premiers = new List<int>();
+
+    ////2 est le premier nombre premier
+    //for (int count = 0, valeur = 2; count < nombre; valeur++)
+    //{
+    //    bool estPremier = true;
+    //    int racine = (int)Math.Sqrt(valeur);
+
+    //    for (int index = 0; estPremier && racine > 1 && premiers[index] <= racine; index++)
+    //    {
+    //        if (valeur % premiers[index] == 0)
+    //            estPremier = false;
+    //    }
+
+    //    if (estPremier)
+    //    {
+    //        premiers.Add(valeur);
+    //        count++;
+    //    }
+    //}
+
+    //foreach (int premier in premiers)
+    //{
+    //    Console.WriteLine($"{premier} est un nombre premier!");
+    //}
+}
+#endregion
+
+#region 1.3 Addition
+{
+    //string nombre1;
+    //string nombre2;
+
+    //do
+    //{
+    //    Console.Write("Entrez un nombre entier : ");
+    //} while (!Regex.IsMatch(nombre1 = Console.ReadLine()!, @"^\d+$"));
+
+    //do
+    //{
+    //    Console.Write("Entrez un second nombre entier : ");
+    //} while (!Regex.IsMatch(nombre2 = Console.ReadLine()!, @"^\d+$"));
+
+    //Stack<int> chiffresNombre1 = new Stack<int>();
+    //foreach (char c in nombre1)
+    //{
+    //    chiffresNombre1.Push(c - 48);
+    //}
+
+    //Stack<int> chiffresNombre2 = new Stack<int>();
+    //foreach (char c in nombre2)
+    //{
+    //    chiffresNombre2.Push(c - 48);
+    //}
+
+    //string resultat = "";
+    //int report = 0;
+
+    //while (chiffresNombre1.Count > 0 || chiffresNombre2.Count > 0)
+    //{
+    //    int chiffre1 = chiffresNombre1.Count > 0 ? chiffresNombre1.Pop() : 0;
+    //    int chiffre2 = chiffresNombre2.Count > 0 ? chiffresNombre2.Pop() : 0;
+
+    //    int addition = chiffre1 + chiffre2 + report;
+    //    resultat = $"{addition % 10}{resultat}";
+    //    report = addition / 10;
+    //}
+
+    //if (report > 0)
+    //{
+    //    resultat = $"{report}{resultat}";
+    //}
+
+    //Console.WriteLine(resultat);
+}
+#endregion
+
+#region Fonction de tri
+{
+    //int nombre;
+
+    //do
+    //{
+    //    Console.Write("Entrez le nombre d'éléménts entiers de votre tableau (au moins 10) : ");
+    //} while (!int.TryParse(Console.ReadLine(), out nombre) || nombre < 10);
+
+    //int[] tableau = new int[nombre];
+
+    //for (int i = 0; i < tableau.Length; i++)
+    //{
+    //    tableau[i] = Random.Shared.Next(1000);
+    //}
+
+    //Console.WriteLine(string.Join(", ", tableau));
+
+    ////Tri simple
+    //for (int i = 0; i < tableau.Length - 1; i++)
+    //{
+    //    for (int j = i + 1; j < tableau.Length; j++)
+    //    {
+    //        if (tableau[j] < tableau[i])
+    //        {
+    //            //swap
+    //            int temp = tableau[j];
+    //            tableau[j] = tableau[i];
+    //            tableau[i] = temp;
+    //        }
+    //    }
+    //}
+
+    //Console.WriteLine(string.Join(", ", tableau));
+}
+#endregion
--- Exo_Correction/Program.cs
+++ Exo_Correction/Program.cs
@@ -5,49 +5,49 @@
 
 // 1.1 : Grâce à une boucle « while » et à l’aide d’une collection, calculez les nombres premiers inférieur à un nombre entier entré au clavier.
 
-using System.Collections;
+//using System.Collections;
 
-int userchoice;
-do
-    Console.WriteLine("entrez le nombre jusqu'auquel nous devons afficher les nombres premiers existants");
+//int userchoice;
+//do
+//    Console.WriteLine("entrez le nombre jusqu'auquel nous devons afficher les nombres premiers existants");
 
-while (!int.TryParse(Console.ReadLine(), out userchoice));
+//while (!int.TryParse(Console.ReadLine(), out userchoice));
 
-Console.WriteLine($"nous allons afficher les nombres premiers jusque {userchoice} ");
-Thread.Sleep(2000);
-Console.Clear();
+//Console.WriteLine($"nous allons afficher les nombres premiers jusque {userchoice} ");
+//Thread.Sleep(2000);
+//Console.Clear();
 
-int compteur = 0;
-ArrayList premiers = new ArrayList();
+//int compteur = 0;
+//ArrayList premiers = new ArrayList();
 
-// 2 est le premier nombre premier
-int valeur = 2;
-while (valeur < userchoice)
-{
-    bool estPremier = true;
-    int racine = (int)Math.Sqrt(valeur);
+//// 2 est le premier nombre premier
+//int valeur = 2;
+//while (valeur < userchoice)
+//{
+//    bool estPremier = true;
+//    int racine = (int)Math.Sqrt(valeur);
 
-    for (int index = 0; estPremier && racine > 1 && (int)premiers[index]! <= racine; index++)
-    {
-        compteur++;
-        if (valeur % (int)premiers[index]! == 0) // unboxing object > int
-            estPremier = false;
-    }
+//    for (int index = 0; estPremier && racine > 1 && (int)premiers[index]! <= racine; index++)
+//    {
+//        compteur++;
+//        if (valeur % (int)premiers[index]! == 0) // unboxing object > int
+//            estPremier = false;
+//    }
 
-    if (estPremier)
-    {
-        premiers.Add(valeur); // boxing int > object
-    }
+//    if (estPremier)
+//    {
+//        premiers.Add(valeur); // boxing int > object
+//    }
 
-    valeur++;
-}
+//    valeur++;
+//}
 
-foreach (int premier in premiers)
-{
-    Console.WriteLine($"{premier} est un nombre premier !");
-}
+//foreach (int premier in premiers)
+//{
+//    Console.WriteLine($"{premier} est un nombre premier !");
+//}
 
-Console.WriteLine($"nombre de boucles : {compteur}");
+//Console.WriteLine($"nombre de boucles : {compteur}");
 
 #endregion
 
@@ -55,57 +55,51 @@ Console.WriteLine($"nombre de boucles : {compteur}");
 
 // 1.2 : Grâce à une boucle « for » et à l’aide d’une collection générique, calculez les x premiers nombres premiers (version optimisée).
 
-int userchoice;
-do
-    Console.WriteLine("Combien de nombres premiers voulez vous trouver ? ");
 
-while (!int.TryParse(Console.ReadLine(), out userchoice));
 
-List<int> nombresPremiers = new List<int>(); // Collection pour stocker les nombres premiers trouvés
-nombresPremiers.Add(2); // Ajouter le premier nombre premier (2)
+//int userchoice;
+//do
+//    Console.WriteLine("entrez le nombre jusqu'auquel nous devons afficher les nombres premiers existants");
 
-for (int i = 1; i < userchoice; i++) // Boucle pour trouver les x premiers nombres premiers
-{
-    int dernierNombrePremier = nombresPremiers[nombresPremiers.Count - 1]; // Récupérer le dernier nombre premier dans la collection
-    int prochainNombrePremier = dernierNombrePremier + 1; // Initialiser le prochain nombre premier à tester
-    bool estPremier = false; // Initialiser la variable à false
+//while (!int.TryParse(Console.ReadLine(), out userchoice));
 
-    while (!estPremier) // Boucle tant que le prochain nombre n'est pas premier
-    {
-        estPremier = true; // Supposer que le nombre est premier
+//Console.WriteLine($"nous allons afficher les nombres premiers jusque {userchoice} ");
+//Thread.Sleep(2000);
+//Console.Clear();
 
-        for (int j = 2; j <= Math.Sqrt(prochainNombrePremier); j++) // Tester si le nombre est premier en utilisant la racine carrée
-        {
-            if (prochainNombrePremier % j == 0)
-            {
-                estPremier = false; // Si le nombre n'est pas premier, mettre la variable à false
-                break; // Sortir de la boucle for pour le test de primalité
-            }
-        }
+//List <int> premiers = new List<int>();
 
-        if (estPremier) // Si le nombre est premier, l'ajouter à la collection
-        {
-            nombresPremiers.Add(prochainNombrePremier);
-        }
-        else // Sinon, passer au nombre suivant
-        {
-            prochainNombrePremier++;
-        }
-    }
-}
-// Afficher les nombres premiers trouvés
-Console.Write("Les " + userchoice + " premiers nombres premiers sont : ");
-foreach (int nombrePremier in nombresPremiers)
-{
-    Console.Write(nombrePremier + " ");
-}
-Console.WriteLine();
+//// 2 est le premier nombre premier
+//int valeur = 2;
+//while (valeur < userchoice)
+//{
+//    bool estPremier = true;
+//    int racine = (int)Math.Sqrt(valeur);
+
+//    for (int index = 0; estPremier && racine > 1 && premiers[index]! <= racine; index++)
+//    {
+//        if (valeur % premiers[index]! == 0)
+//            estPremier = false;
+//    }
+
+//    if (estPremier)
+//    {
+//        premiers.Add(valeur);
+//    }
+
+//    valeur++;
+//}
+
+//foreach (int premier in premiers)
+//{
+//    Console.WriteLine($"{premier} est un nombre premier !");
+//}
 
 #endregion
 
 #region Exo 1.3
 
-//1.3 : Demandez à l’utilisateur d’introduire deux nombres au clavier et faite l’addition de ces deux nombres en ne convertissant que ca ractère par caractère. (Méthode « ToCharArray » de la classe « string »).
+//1.3 : Demandez à l’utilisateur d’introduire deux nombres au clavier et faite l’addition de ces deux nombres en ne convertissant que caractère par caractère. (Méthode « ToCharArray » de la classe « string »).
 
 
 
